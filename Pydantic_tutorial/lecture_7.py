bc58d366d8bd5b5bb47d9e3ed2262c2592c483c2
imashwini09 <ashwinisamal9@gmail.com>
2026-03-31 09:59:10 +0530

    pydantic

---
+++ Pydantic_tutorial/lecture_7.py
@@ -0,0 +1,64 @@
+from pydantic import BaseModel, ValidationError, Field, EmailStr, HttpUrl, SecretStr
+from datetime import datetime, UTC
+from functools import partial
+from uuid import UUID, uuid4
+from typing import Literal,Annotated
+
+class User(BaseModel):
+    uid : UUID = Field(default_factory=uuid4)   
+    username : Annotated[str, Field(min_length=3, max_length=20)]
+    email : EmailStr
+    age : Annotated[int, Field(ge=13,le=130)]
+
+    website : HttpUrl | None = None
+    password : SecretStr
+
+    verified_at : datetime | None = None
+
+    bio : str = ""
+    is_active : bool = True
+
+    full_name : str | None = None
+
+class BlogPost(BaseModel):
+    title : Annotated[str, Field(min_length=1,max_length=200)]
+    content : Annotated[str, Field(min_length=20)]
+    view_count : int = 0
+    is_published : bool = False
+
+    tags : list[str] = Field(default_factory=list)
+
+    # create_at : datetime = Field(default_factory=lambda : datetime.now(tz=UTC))
+    # both will do same work lambda or partial function
+    create_at : datetime = Field(default_factory=partial(datetime.now,tz=UTC))
+
+    author_id : str | int
+    status : Literal["draft", "published", "archived"] = "draft"
+
+    slug : Annotated[str, Field(pattern=r"^[a-z0-9-]+$")]
+
+
+user = User(
+        username="Ashwini",
+        email="ashwini@gmail.com",
+        age=20,
+        password="mysecretpassword",
+
+    )
+print(user)
+print(user.password.get_secret_value())
+
+# Password will not be printed in the output because 
+# it is of type SecretStr and it is designed to hide the actual value when printed. 
+# Instead, it will show a placeholder like "**********" to indicate that the value is hidden.
+# To access the actual value of the password, you can use the get_secret_value() method, 
+# which will return the original string value of the password.  
+
+
+# post = BlogPost(
+#     title="Getting started with Python",
+#     content="python is dynamically typing language.",
+#     author_id="123",
+# )
+
+# print(post)
